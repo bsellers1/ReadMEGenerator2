7a7f70b1d7cbdc2fafd07bff30b23389d2321699
Brian Sellers <bsellers3013@gmail.com>
2023-07-04 04:29:27 -0500

    added badges and also makes users enter info

--- sample.js
+++ sample.js
@@ -1,5 +1,38 @@
+const fs = require('fs');
+
+function renderLicenseBadge(license) {
+    if (!license) {
+        return ``;
+    } else {
+        return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+    }
+}
+
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+        return `https://lbesson.mit-license.org/`
+    }
+    if (license === 'GPL') {
+        return `http://perso.crans.org/besson/LICENSE.html`
+    }
+    if (license === 'Apache') {
+        return `https://www.apache.org/licenses/LICENSE-2.0`
+    }
+    if (license === 'Eclipse') {
+        return `https://www.eclipse.org/legal/epl-v10.html`
+    }
+}
+
+function renderLicenseSection(license) {
+    if (!license) {
+        return ``;
+    } else {
+        return `This project is licensed under the ${license} license.`
+    }
+}
+
 function sample({ title, description, license, installation, usage, contribution, github, email }) {
-return `# ${title}
+    return `# ${title}
 
 ## Description
 - ${description}  
@@ -25,7 +58,8 @@ return `# ${title}
 - GitHub: ${github}
 
 ## License
-- ${license}
+${renderLicenseBadge(license)}
+- ${renderLicenseSection(license)}
     `;
 }
 
